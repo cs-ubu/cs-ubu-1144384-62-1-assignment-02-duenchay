f5b3a5961b043c35c48d39fa870f2a1d68065313
duenchay <duenchay.wo.60@ubu.ac.th>
2019-07-12 15:07:50 +0700

    eiei55

---
+++ galleryapp/index.ts
@@ -0,0 +1,32 @@
+import { app, BrowserWindow } from 'electron'
+import { create } from 'domain';
+
+let win :any  = null
+
+function createWindow() {
+  win = new BrowserWindow({width: 500, height: 400})
+  win.setTitle('CSUBU Lecturers')
+  win.loadFile('index.html')
+  win.on('closed', () => { win = null })
+}
+
+app.on('ready', () => {
+    console.log('app น่ะ  ready แล้วนะ ')
+    createWindow()
+})
+
+// สำหรับ macOS 
+app.on('window-all-closed', function () {
+  // ถ้าหน้าต่างทุกอันปิด
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+// ถ้ากด icon ของ app ใหม่
+app.on('activate', function () {
+    
+  if (win == null) {
+    createWindow()
+  }
+})
